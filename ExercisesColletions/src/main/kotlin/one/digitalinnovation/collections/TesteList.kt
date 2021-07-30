f7e3cf3323b21a1e85a6563f73201d1bf509b90b
hiltoncsj <hiltoncsj@gmail.com>
2021-07-30 01:48:07 -0300

    created arrayList of test class

---
+++ ExercisesColletions/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,34 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario("João", 2500.0, "CLT")
+    val joana = Funcionario("Joana", 6500.0, "PJ")
+    val mario = Funcionario("Mário", 5500.0, "CLT")
+
+    val funcionarios = listOf(joao, mario, joana)
+    funcionarios.forEach { println(it) }
+
+    println("------------------------")
+    println( funcionarios.find { it.nome == "Maria" } )
+
+    println("------------------------")
+    println( funcionarios.find { it.nome == "Mário" } )
+
+    println("------------------------")
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+
+    println("------------------------")
+    funcionarios.groupBy { it.tipoContratacao }.forEach { println(it) }
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+    override fun toString(): String =
+    """
+        Nome: $nome
+        Salário: $salario
+    """.trimIndent()
+}
